f2aa9b24fb0d0b9ef366e157dfc0f3f1ddc7ac6c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-26 19:51:11 -0400

    chore: get single Project by id

--- server/api/project.js
+++ server/api/project.js
@@ -10,3 +10,17 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let projectId = req.params.id
+    const singleProject = await Project.findByPk(projectId)
+    if (!singleProject) {
+      res.status(404).json('No Project Found')
+    } else {
+      res.status(200).json(singleProject)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
