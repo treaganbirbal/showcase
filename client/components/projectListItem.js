3e0e3506d7611d12792734f761453d1c12c109e3
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-27 15:02:14 -0400

    fix:error with routing to singleProject.  Feature: Basic style

--- client/components/projectListItem.js
+++ client/components/projectListItem.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ProjectListItem = props => {
   console.log('props from List: ', props)
   return props.projects.map(project => {
     return (
       <div className="list-ctn" key={project.id}>
-        <h1>{project.name}</h1>
-        <p>{project.description}</p>
-        <img
-          src={project.imageUrl}
-          alt="project-image"
-          width="500"
-          height="200"
-        />
+        <Link to={`/projects/${project.id}`}>
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <img
+            src={project.imageUrl}
+            alt="project-image"
+            width="500"
+            height="200"
+          />
+        </Link>
       </div>
     )
   })
