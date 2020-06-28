4c96b3bdc87faf65d6da2b196baba152e481e9aa
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-27 21:42:03 -0400

    chore: create userProfile to display of of the work

--- client/store/allUsers.js
+++ client/store/allUsers.js
@@ -3,12 +3,15 @@ import history from '../history'
 
 //action types
 const GET_USERS = 'GET_USERS'
+const GOT_USER = 'GOT_USER'
 
 //action creators
 const getUsers = requestedUsers => ({
   type: GET_USERS,
   requestedUsers
 })
+
+const gotUser = requestedUser => ({type: GOT_USER, requestedUser})
 
 //state
 const initialUsers = []
@@ -25,11 +28,23 @@ export const fetchUsers = () => {
   }
 }
 
+export const fetchUser = id => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/users/${id}`)
+    console.log('data', data)
+    dispatch(gotUser(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 //reducer
 export default function(state = initialUsers, action) {
   switch (action.type) {
     case GET_USERS:
       return action.requestedUsers
+    case GOT_USER:
+      return action.requestedUser
     default:
       return state
   }
